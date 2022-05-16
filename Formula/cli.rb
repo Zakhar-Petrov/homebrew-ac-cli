35241769a5b0a15cc0d3b96d252fbc43359b0c23
Zakhar Petrov <46595458+zakhar-petrov@users.noreply.github.com>
2022-05-17 00:53:51 +0300

    Update ac-cli version

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,9 +1,9 @@
 class Cli < Formula
   desc "Agisoft Cloud CLI"
   homepage "https://github.com/Zakhar-Petrov/agisoft-cloud-cli"
-  url "https://github.com/Zakhar-Petrov/homebrew-agisoft-cloud/releases/download/v1.0.0/ac-cli-macos-x64.tar.gz"
-  sha256 "41f89dacecc91e1755476070ec5061fa12f17b2fb0c909203b3ef714eeb93552"
-  version "1.0.0"
+  url "https://github.com/Zakhar-Petrov/homebrew-agisoft-cloud/releases/download/v1.1.0/ac-cli-macos-x64.tar.gz"
+  sha256 "40a72b6c1801024f9786b9af96c30ff5d8645947a82095ae6ea6548f4a7e4cd5"
+  version "1.1.0"
   def install
     bin.install "ac-cli"
   end
